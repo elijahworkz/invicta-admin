41dcf2d85a08663b4afd8714fcb975097e77b08d
Ilya Gromov <elijahworkz@gmail.com>
2022-09-11 22:13:01 +0300

    feat: added textEditor field

---
+++ resources/js/components/form/TextEditorBase.vue
@@ -0,0 +1,41 @@
+<template>
+	<div class="ckeditor">
+		<ckeditor ref="ckeditor" @ready="onReady" :editor="editor" v-model="editorValue" />
+	</div>
+</template>
+
+<script>
+import CkeditorVue from '@ckeditor/ckeditor5-vue'
+import ClassicEditor from 'ckeditor5-build-invicta'
+
+export default {
+	name: 'TextEditorBase',
+
+	props: {
+		initialValue: String
+	},
+
+	components: {
+		ckeditor: CkeditorVue.component
+	},
+
+	data() {
+		return {
+			editor: ClassicEditor,
+			editorValue: ''
+		}
+	},
+
+	watch: {
+		editorValue(newValue) {
+			this.$emit('updated', newValue)
+		} 
+	},
+
+	methods: {
+		onReady() {
+			this.editorValue = this.initialValue
+		}
+	}
+}
+</script>
