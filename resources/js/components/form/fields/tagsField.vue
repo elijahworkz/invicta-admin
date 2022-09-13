28391e6e1c3967a4e158ffe75868225ca3dc2d20
Ilya Gromov <elijahworkz@gmail.com>
2022-09-13 11:20:59 +0300

    feat: added tagsField
    fixes #1

---
+++ resources/js/components/form/fields/tagsField.vue
@@ -0,0 +1,117 @@
+<template>
+	<FieldBase :form-id="formId" :field-props="props">
+		<div class="tags-field">
+			<div class="flex items-center mb-3">
+				<el-input v-model="newTag" placeholder="Add Tag..." class="mr-2" @keydown="handleEnter" />
+			</div>
+			<div class="tags-list">		
+				<span class="tag" v-for="(tag, index) in tags" :key="index">
+					<span>{{ tag }}</span>
+					<el-icon class="remove-tag" @click="removeTag(tag)" title="Remove Tag"><Close/></el-icon>
+				</span>
+			</div>
+		</div>
+	</FieldBase>
+</template>
+
+<script setup>
+import { ref, computed } from 'vue'
+import FieldBase from '@/components/form/FieldBase.vue'
+import { useFormField } from '@/services/form/field'
+import { onKeyStroke } from '@vueuse/core'
+import { Close } from '@element-plus/icons-vue'
+
+const props = defineProps({
+	formId: String,
+	data: Object,
+	path: String
+})
+const field = useFormField(props)
+const fieldValue = field.value()
+
+const newTag = ref('')
+
+const tags = computed(() => {
+	return fieldValue.value === null ? [] : fieldValue.value
+})
+
+const addTag = () => {
+	let tag = newTag.value.trim()
+	if (tag.length === 0 || tags.value.includes(tag)) {
+		return
+	}
+	fieldValue.value = [...tags.value, tag]
+	newTag.value = ''
+}
+
+const removeTag = (tag) => {
+	fieldValue.value = tags.value.filter(t => t !== tag)
+}
+
+const handleEnter = (event) => {
+	if (event.keyCode === 13) {
+		console.log('got enter event', event)
+		event.stopPropagation()
+		addTag()
+	}
+}
+
+// onKeyStroke('Enter', (e) => {
+// 	e.preventDefault()
+// 	addTag()
+// })
+</script>
+
+<style lang="scss">
+.tags-input {
+	display: flex;
+	flex-wrap: wrap;
+	background-color: #fff;
+	border-width: 1px;
+	border-radius: .25rem;
+	padding-left: .5rem;
+	padding-right: 1rem;
+	padding-top: .5rem;
+	padding-bottom: .25rem;
+	
+
+
+	.tag-input {
+		flex: 1;
+		outline: 0;
+		padding-top: .25rem;
+		padding-bottom: .25rem;
+		margin-left: .5rem;
+		margin-bottom: .25rem;
+		min-width: 10rem;
+	}
+}
+.tags-list {
+	.tag {
+		line-height: 1;
+		display: inline-flex;
+		align-items: center;
+		background-color: var(--el-color-primary-light-9);
+		border-radius: .25rem;
+		user-select: none;
+		padding: 0.5rem 0.3rem 0.5rem 0.6rem;
+		margin-right: .5rem;
+		margin-bottom: .25rem;
+		
+		&:last-of-type {
+			margin-right: 0;
+		}
+	}
+
+	.remove-tag {
+		color: var(--el-color-primary);
+		line-height: 1;
+		margin-left: .25rem;
+		cursor: pointer;
+
+		&:focus {
+			outline: 0;
+		}
+	}	
+}
+</style>
