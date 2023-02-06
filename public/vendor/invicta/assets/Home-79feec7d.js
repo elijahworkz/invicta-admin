3ea815ee9cf48fb601d3e6c0c51cfbf538e0bd39
Ilya Gromov <ilyag@eteachergroup.com>
2023-02-06 19:41:03 +0300

    build

--- public/vendor/invicta/assets/Home-79feec7d.js
+++
@@ -1,1 +0,0 @@
-import{_ as t}from"./_plugin-vue_export-helper-c27b6911.js";import{l as a,y as s,t as o,F as r,g as n,b as c}from"./runtime-core.esm-bundler-83f11067.js";const l={name:"Home",data(){return{}}},i=n("h1",null,"THis will be first test",-1);function p(e,m,_,d,f,u){return c(),a(r,null,[i,s(" "+o(e.$page),1)],64)}const h=t(l,[["render",p]]);export{h as default};
