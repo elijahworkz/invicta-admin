e73110e89ce5b6600c4800117fe0576c1f22ccab
Ilya Gromov <ilyag@eteachergroup.com>
2023-07-23 17:59:56 +0200

    feat: title clickable for available for navigation resources in edit

--- resources/js/components/form/FormBase.vue
+++ resources/js/components/form/FormBase.vue
@@ -9,10 +9,13 @@
 					:href="breadcrumb.url" 
 					class="breadcrumb">
 					<el-icon><ArrowLeft /></el-icon> {{ breadcrumb.text }}</Link>
-				<h1 class="mb-1 flex items-center" v-html="resourceForm.title"></h1>
-				<span class="text-gray-400" v-if="resourceForm.meta.itemUrl">
-					<a href="{{ resourceForm.meta.itemUrl }}" target="_blank">{{ resourceForm.meta.itemUrl }}</a>
-				</span>
+				<h1 class="mb-1">
+					<a v-if="resourceForm.meta.itemUrl" class="flex items-center" :href="resourceForm.meta.itemUrl" title="Visit URL">
+						<span v-html="resourceForm.title"></span>
+						<svg class="ml-2" viewBox="0 0 24 24" width="22"><path fill="currentColor" d="M14,3V5H17.59L7.76,14.83L9.17,16.24L19,6.41V10H21V3M19,19H5V5H12V3H5C3.89,3 3,3.9 3,5V19A2,2 0 0,0 5,21H19A2,2 0 0,0 21,19V12H19V19Z"></path></svg>
+					</a>
+					<span v-else class="flex items-center" v-html="resourceForm.title"></span>
+				</h1>
 			</div>
 			<div class="resource-actions flex items-center ml-auto">
 				<Localizations 
