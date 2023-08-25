924f175b88d03c9ab7c6cc97fdad65b8e79c878c
Ilya Gromov <ilyag@eteachergroup.com>
2023-08-25 16:59:14 +0200

    fix: add missing use

--- routes/webhooks.php
+++ routes/webhooks.php
@@ -1,5 +1,7 @@
 <?php
 
+use Eteacher\InvictaAdmin\Http\Controllers\DeploymentController;
+
 if (config('invicta.deployment.driver') == 'codepipeline') {
     Route::post('codepipeline', [DeploymentController::class, 'processWebhook']);
 }
