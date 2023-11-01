1abc1ffe26605de23d2869fe6b48dc348c537a1d
Ilya Gromov <ilyag@eteachergroup.com>
2023-11-01 16:30:10 +0100

    chore: build

---
+++ public/vendor/invicta/assets/vue.runtime.esm-bundler-3ef88106.js
@@ -0,0 +1,1 @@
+import{b2 as e,bj as r}from"./vendor-7d8317bd.js";const s=e(r);export{s as r};
--- public/vendor/invicta/assets/vue.runtime.esm-bundler-b3f6c0a0.js
+++
@@ -1,1 +0,0 @@
-import{bn as n,bo as a}from"./vendor-0fe75759.js";const _=(e,r)=>{const t=e.__vccOpts||e;for(const[o,s]of r)t[o]=s;return t},m=n(a);export{_,m as r};
