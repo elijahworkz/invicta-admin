4da05fb00f4a735c79a491c11b2c70ae27aca69d
Ilya Gromov <ilyag@eteachergroup.com>
2024-03-19 15:26:38 +0100

    chore: style fix

--- src/Http/Controllers/Api/ResourceController.php
+++ src/Http/Controllers/Api/ResourceController.php
@@ -76,7 +76,7 @@ class ResourceController extends Controller
 
         return collect($request->resourceClass()->allActions())
             ->filter(function ($action) use ($handle) {
-                if (!$action->inline()) {
+                if (! $action->inline()) {
                     if ($action->redirect) {
                         $action->setRedirect($handle);
                     }
@@ -125,7 +125,7 @@ class ResourceController extends Controller
 
     public function destroy(ResourceRequest $request)
     {
-        $this->authorize('delete ' . $request->handle());
+        $this->authorize('delete '.$request->handle());
 
         // ResourceRequest $request
         $resource = $request->resourceClass();
@@ -137,7 +137,7 @@ class ResourceController extends Controller
         return response()->json([
             'message' => [
                 'type' => 'success',
-                'title' => 'Selected ' . $resource->handle() . ' deleted',
+                'title' => 'Selected '.$resource->handle().' deleted',
             ],
         ]);
     }
