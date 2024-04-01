08ec0d9b048109d5c2ddd38b305c76887a7b5112
Ilya Gromov <ilyag@eteachergroup.com>
2024-04-01 09:24:11 +0200

    chore: build

--- resources/js/components/form/fields/codeField.vue
+++ resources/js/components/form/fields/codeField.vue
@@ -1,52 +1,61 @@
 <template>
-	<FieldBase :form-id="formId" :field-props="props" :class="{'is-error': jsonError}">
-		<codemirror
-			v-model="stringValue"
-			:style="{height: 'auto', width: '100%'}"
-			:autofocus="true"
-			:indent-with-tab="true"
-			:tab-size="4"
-			:extensions="extensions"
-		/>
-	</FieldBase>
+    <FieldBase
+        :form-id="formId"
+        :field-props="props"
+        :class="{ 'is-error': jsonError }"
+    >
+        <div class="code-wrapper">
+            <codemirror
+                v-model="stringValue"
+                :style="{ height: 'auto', width: '100%' }"
+                :autofocus="true"
+                :indent-with-tab="true"
+                :tab-size="4"
+                :extensions="extensions"
+            />
+        </div>
+    </FieldBase>
 </template>
 
 <script setup>
-import { Codemirror } from 'vue-codemirror'
-import { json } from '@codemirror/lang-json'
-import { oneDark } from '@codemirror/theme-one-dark'
+import { Codemirror } from "vue-codemirror";
+import { json } from "@codemirror/lang-json";
+import { oneDark } from "@codemirror/theme-one-dark";
 // import { ElMessage } from 'element-plus'
 
 const props = defineProps({
-	formId: String,
-	data: Object,
-	path: String
-})
-const field = useFormField(props)
-const value = field.value()
+    formId: String,
+    data: Object,
+    path: String,
+});
+const field = useFormField(props);
+const value = field.value();
 
-const jsonError = ref(false)
+const jsonError = ref(false);
 
-const stringValue = ref(JSON.stringify(value.value, null, "\t"))
+const stringValue = ref(JSON.stringify(value.value, null, "\t"));
 
-watch(stringValue, debounce(_code => {
-	try { 
-		value.value = JSON.parse(_code)
-		jsonError.value = false
-	} catch (error) {
-		jsonError.value = true
-		ElMessage.error('You have some errors in your JSON')
-		console.log('error in json', error.message)
-	}
-}, 500))
+watch(
+    stringValue,
+    debounce((_code) => {
+        try {
+            value.value = JSON.parse(_code);
+            jsonError.value = false;
+        } catch (error) {
+            jsonError.value = true;
+            ElMessage.error("You have some errors in your JSON");
+            console.log("error in json", error.message);
+        }
+    }, 500),
+);
 
-const extensions = [json(), oneDark]
+const extensions = [json(), oneDark];
 </script>
 
 <style lang="scss">
 .cm-editor {
-	.is-error  & {
-		border: 2px solid var(--el-color-danger)
-	}
+    .is-error & {
+        border: 2px solid var(--el-color-danger);
+    }
 }
 </style>
