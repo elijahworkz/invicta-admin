4111b15fd7524a7fb437c559405d3942bc2144b9
Yulia.s <yulia.s@eteachergroup.com>
2024-05-28 12:57:00 -0400

    chore: update checkboxgroup + comment overflow hidden for for + make shorter title of the form

--- resources/js/components/form/fields/checkboxGroupField.vue
+++ resources/js/components/form/fields/checkboxGroupField.vue
@@ -1,41 +1,44 @@
 <template>
-	<FieldBase :form-id="formId" :field-props="props" class="checkbox-group" :class="{'stacked': data.stacked}">
-		<el-checkbox v-for="(item, index) in value" v-model="value[index]" :label="options[index]" />
+	<FieldBase :form-id="formId" :field-props="props" :style="{'--grid-column': `repeat(${columns}, 1fr)` }">
+    <el-checkbox-group v-model="fieldValue">
+      <el-checkbox v-for="(option) in options" :value="option[valueField]" :label="option[labelField]"/>
+    </el-checkbox-group>
 	</FieldBase>
 </template>
 
 <script setup>
 const props = defineProps({
-	formId: String,
-	data: Object,
-	path: String
+  formId: String,
+  data: Object,
+  path: String
 })
+
+const resourceForm = useResourceForm(props.formId)
 const field = useFormField(props)
-const value = field.value()
-
+const fieldValue = field.value([])
 const options = ref([])
+const columns = props.data.columns ?? 1
 
-onMounted(() => {
+const valueField = props.data.valueField || 'value'
+const labelField = props.data.labelField || 'label'
 
-	if (Array.isArray(props.data.options)) {
-		options.value = props.data.options
-	} else {
+if (Array.isArray(props.data.options)) {
+  options.value = props.data.options
+} else {
+  options.value = resourceForm.remoteData.get(props.data.options)
 
-		Invicta.axios.get(props.data.options)
-			.then(({data}) => {
-				options.value = data
-			})
-	}
-})
+  if (! Array.isArray(options.value)) {
+    Invicta.on(props.data.options, () => {
+      options.value = resourceForm.remoteData.get(props.data.options)
+    })
+  }
+}
+
 </script>
 
 <style lang="scss">
-.checkbox-group {
-	&.stacked {
-		.el-checkbox {
-			width: 100%;
-		}
-	}
-}
+  .el-checkbox-group {
+    display: grid;
+    grid-template-columns: var(--grid-column);
+  }
 </style>
-
